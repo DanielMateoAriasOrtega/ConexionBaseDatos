8d76498b19954a412c04a12c16108c45a1aa2935
DanielMateoAriasOrtega <dmateo.arias@gmail.com>
2026-05-04 01:12:00 -0500

    Fix string quotes in db.js

--- server/db.js
+++ server/db.js
@@ -1,21 +1,26 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const mysql = require("mysql2");
+const mysql = require('mysql2');
+
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "docentes_db",
+
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'docentes_db'
+
 });
+
 
 connection.connect((err) => {
-  if (err) {
-    console.log("Error al conectar la bd", err);
-    return;
-  }
+    if (err) {
+        console.log('Error al conectar la bd', err);
+        return;
+    }
 
-  console.log("Conexion exitosa .....");
+    console.log('Conexion exitosa .....');
 });
+
 
 module.exports = connection;
